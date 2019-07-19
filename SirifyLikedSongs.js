70b5ed06b48dd9824bdc10ef586ccf0f3a822b91
David Miller <davidjackmiller@gmail.com>
2019-07-19 17:15:57 -0400

    fetching refresh token

--- SirifyLikedSongs.js
+++ SirifyLikedSongs.js
@@ -5,43 +5,55 @@ let cred = clientID + ':' + clientSecret
 let credEncoded = Data.fromString(cred).toBase64String()
 let auth = 'Basic ' + credEncoded
 
-let token = THE_REFRESH_TOKEN
+// Find the refresh token
+var ref_token = ""
+Contact.all(ContactsContainer.all()).then(function(contacts) {
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].givenName == "Sirify") {
+            ref_token = contacts[i].note
+            break
+        }
+    }
+    continueExecution()
+})
 
-// Get a new access token
-let tokenURL = 'https://accounts.spotify.com/api/token'
-var reqToken = new Request(tokenURL)
-reqToken.method = 'POST'
-reqToken.body = 'grant_type=refresh_token&refresh_token=' + token
-reqToken.headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Authorization': auth
+function continueExecution() {
+    // Get a new access token
+    let tokenURL = 'https://accounts.spotify.com/api/token'
+    var reqToken = new Request(tokenURL)
+    reqToken.method = 'POST'
+    reqToken.body = 'grant_type=refresh_token&refresh_token=' + ref_token
+    reqToken.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': auth
+    }
+
+    let res = await reqToken.loadJSON()
+    let token = res['access_token']
+    let auth2 = 'Bearer ' + token
+
+    // Params
+    var q = encodeURI(URLScheme.parameter('query'))
+    var t = URLScheme.parameter('type')
+
+    let endpoint = 'https://api.spotify.com/v1/me/tracks'
+    let method = 'GET'
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': auth2
+    }
+
+    // Make the request
+    let req = new Request(endpoint)
+    req.method = method
+    req.headers = headers
+
+    // Get the track URL
+    let json = await req.loadJSON()
+
+    console.log(json)
+
+    // Open the track on Spotify
+    //Safari.open(finalURL)
 }
-
-let res = await reqToken.loadJSON()
-let token = res['access_token']
-let auth2 = 'Bearer ' + token
-
-// Params
-var q = encodeURI(URLScheme.parameter('query'))
-var t = URLScheme.parameter('type')
-
-let endpoint = 'https://api.spotify.com/v1/me/tracks'
-let method = 'GET'
-let headers = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  'Authorization': auth2
-}
-
-// Make the request
-let req = new Request(endpoint)
-req.method = method
-req.headers = headers
-
-// Get the track URL
-let json = await req.loadJSON()
-
-console.log(json)
-
-// Open the track on Spotify
-//Safari.open(finalURL)
